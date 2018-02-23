ab5f1e8558307c45e94c05712143ac87abda99cc
Laurent-St <laurent-st@hotmail.com>
2018-02-23 11:06:04 +0100

    correction permittivité relative
    donne de meilleurs resultats et une symmetrie
    + on a bougé la source et la plaque dans knife edge pour mieux voir

--- knife_edge.m
+++ knife_edge.m
@@ -5,8 +5,8 @@ c=3e8;
 
 x0 = 0;
 y0 = 0;
-xf = 1;
-yf = 1;
+xf = 2*1;
+yf = 2*1;
 
 x_step = c/1e9/30; %Accuracy 1Ghz
 %en-dessous de x_step = lambda/14 ça ne se propage plus
@@ -19,10 +19,10 @@ x = x0:x_step:xf;
 y = y0:x_step:yf;
 t = t0:t_step:tf;
 
-eps_rel = ones(length(y), length(x));
+eps_rel = ones(length(y), length(x)); 
 mu_rel = ones(length(y), length(x));
 
-eps_rel(1:50,50) = ones(50,1)*1e6; %iron wall on x=50
-mu_rel(1:50,50) = ones(50,1)*5000;
+eps_rel(1:50,90) = ones(50,1)*1; %iron wall on x=90, %ATTENTION PAS 10^6 car c'est eps RELATIF
+mu_rel(1:50,90) = ones(50,1)*5000; %ici c'est bien la bonne valeur de mu relative
 
-E = FDTD_compute(x,y,t,25,25,eps_rel,mu_rel,1,'line([50 50],[0 50]);')
+E = FDTD_compute(x,y,t,75,40,eps_rel,mu_rel,1,'')%,'line([50 90],[0 90]);')
