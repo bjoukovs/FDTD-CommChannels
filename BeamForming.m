b8763c6fc2760638cbb84f3206e4ca3abce62a84
Laurent-St <laurent-st@hotmail.com>
2018-05-03 12:35:31 +0200

    beamforming_to_finish

--- BeamForming.m
+++ BeamForming.m
@@ -7,15 +7,16 @@ c=3e8;
 
 x0 = 0;
 y0 = 0;
-xf = 1;
-yf = 1;
+xf = 1/2;
+yf = 1/2;
 
-x_step = c/(2.45e9)/30; %Accuracy 1Ghz
+f=2.45e9;
+x_step = c/f/30; %Accuracy 1Ghz
 %en-dessous de x_step = lambda/14 ça ne se propage plus
 
 t0 = 0;
 t_step = x_step/c/10; %Stability (1D condition)
-tf = t0 + 10000*t_step;
+tf = t0 + 2000*t_step;
 
 x = x0:x_step:xf;
 y = y0:x_step:yf;
@@ -25,13 +26,18 @@ eps_rel = ones(length(y), length(x));
 mu_rel = ones(length(y), length(x));
 
 %sources
-yl = 10;
-xl = 10;
+yl = 50;
+xl = 40;
 nb_sources = 6;
 sourca = zeros(nb_sources,2);
+spacing=floor((c/f)/4/x_step);
 for i = 1: nb_sources
-    sourca(i,:) = [yl, xl + 10*i];
+    sourca(i,:) = [yl, xl + spacing*i];
 end
 
-E = FDTD_compute_beam_forming(x,y,t,sourca,eps_rel,mu_rel,1,'');
+
+[E, coupe_distance, coupe_temps]=FDTD_compute_beam_forming(x,y,t,sourca,eps_rel,mu_rel,0,'');
+
+figure;plot(y,coupe_distance);
+figure;plot(t,coupe_temps);
 
--- FDTD_compute_beam_forming.m
+++ FDTD_compute_beam_forming.m
@@ -1,14 +1,17 @@
-function Ez = FDTD_compute_beam_forming(x,y,t,sourca,eps_rel,mu_rel,show_movie,custom_display)
+function [Ez,coupe_distance,coupe_temps] = FDTD_compute_beam_forming(x,y,t,sourca,eps_rel,mu_rel,show_movie,custom_display)
+    
+    coupe_temps=zeros(1,length(t)); %power at a specific place (x0,y0) in function of time;
+    coupe_distance=zeros(1,length(y)); %power at a specific instant t0 on all the y's
 
     colormapfile = matfile('hotcoldmap.mat');
     cm = colormapfile.cm;
     cm = cm/255;
 
     %Allocate space for movie
-    if show_movie==1
-        F(length(t)) = struct('cdata',[],'colormap',[]);
-        
-    end
+%     if show_movie==1
+%         F(length(t)) = struct('cdata',[],'colormap',[]);
+%         
+%     end
 
     mu_0 = 4*pi*1e-7;
     eps_0 = 8.85e-12;
@@ -26,12 +29,15 @@ function Ez = FDTD_compute_beam_forming(x,y,t,sourca,eps_rel,mu_rel,show_movie,c
     beta = (eps_rel).^-1 .*(t_step/eps_0/x_step);
     
     [n_sources,~]=size(sourca);
+    p=1:n_sources;
     for i=1:length(t)
 
        %source
-       for p=1:n_sources
-           Ez(sourca(p,1),sourca(p,2))=sin(2*pi*1e9*t(i));
-       end
+%        for p=1:n_sources
+%            Ez(sourca(p,1),sourca(p,2))=sin(2*pi*1e9*t(i));
+%        end
+        
+        Ez(sourca(p,1),sourca(p,2))=sin(2*pi*1e9*t(i));
            
         %Update of Hx, Hy
         for j=1:length(x)
@@ -54,14 +60,33 @@ function Ez = FDTD_compute_beam_forming(x,y,t,sourca,eps_rel,mu_rel,show_movie,c
         %Update of Ez
         for l=2:length(x)
             for m=2:length(y)
-                
-                 if ~(ismember(l,sourca(:,2))) || ~(ismember(m,sourca(:,1)))
-                    Ez(m,l) = Ez(m,l) + beta(m-1,l-1)*(Hy(m,l) - Hy(m,l-1)) - beta(m-1,l-1)*(Hx(m,l)-Hx(m-1,l));
-                 else
-                     l,m;
+               flag=0;
+               for a=1:n_sources
+                 if (l==sourca(a,2)) && (m==sourca(a,1))
+                    flag=1; %to tell if the point is a source
+                    break
                  end
-
+               end
+               if flag==1
+                   l,m;
+               else
+                   Ez(m,l) = Ez(m,l) + beta(m-1,l-1)*(Hy(m,l) - Hy(m,l-1)) - beta(m-1,l-1)*(Hx(m,l)-Hx(m-1,l));
+               end
+               
+%                  if ~(ismember(l,sourca(:,2))) || ~(ismember(m,sourca(:,1)))
+%                     Ez(m,l) = Ez(m,l) + beta(m-1,l-1)*(Hy(m,l) - Hy(m,l-1)) - beta(m-1,l-1)*(Hx(m,l)-Hx(m-1,l));
+%                  else
+%                      l,m;
+%                  end
+                 
+                 if m==60 && l==60
+                     coupe_temps(i)=(Ez(m,l).^2)*0.5;
+                 end
             end
+        end
+        
+        if i==length(t)/4
+            coupe_distance=(Ez(60,:).^2)*0.5;
         end
         
         if show_movie==1
@@ -71,14 +96,14 @@ function Ez = FDTD_compute_beam_forming(x,y,t,sourca,eps_rel,mu_rel,show_movie,c
                 imagesc(Ez, [-1,1])
                 xlabel('x');
                 ylabel('y');
-                xticks(linspace(0,length(x)-1,10));
-                yticks(linspace(0,length(y)-1,10));
-                xticklabels( round(linspace(x(1),x(end),10),2) );
-                yticklabels( round(linspace(y(1),y(end),10),2) );
-                hold on;
+                %xticks(linspace(0,length(x)-1,10));
+                %yticks(linspace(0,length(y)-1,10));
+                %xticklabels( round(linspace(x(1),x(end),10),2) );
+                %yticklabels( round(linspace(y(1),y(end),10),2) );
+                %hold on;
                 %imagesc(eps_rel_draw);
                 eval(custom_display);
-                hold off;
+                %hold off;
                 colorbar;
             
             %draw subplot for colorbar
@@ -90,7 +115,8 @@ function Ez = FDTD_compute_beam_forming(x,y,t,sourca,eps_rel,mu_rel,show_movie,c
                
             
             %Save movie
-            F(i) = getframe(gcf);
+            
+            %F(i) = getframe(gcf);
             
         end
         
@@ -98,13 +124,14 @@ function Ez = FDTD_compute_beam_forming(x,y,t,sourca,eps_rel,mu_rel,show_movie,c
     
     %fig = figure;
     %movie(fig,F,2)
-    filename = 'FDTD_'+datestr(datetime('now'),'ddmmyy_HH_MM')+'.mp4'
-    video = VideoWriter(char(filename),'MPEG-4');
-    video.Quality = 90;
-    open(video);
-    for i=1:length(F)
-       frame = F(i);
-       writeVideo(video,frame);
-    end
-    close(video);
+    
+%     filename = 'FDTD_'+datestr(datetime('now'),'ddmmyy_HH_MM')+'.mp4'
+%     video = VideoWriter(char(filename),'MPEG-4');
+%     video.Quality = 90;
+%     open(video);
+%     for i=1:length(F)
+%        frame = F(i);
+%        writeVideo(video,frame);
+%     end
+%     close(video);
 end
